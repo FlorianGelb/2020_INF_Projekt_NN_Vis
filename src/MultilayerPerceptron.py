16bc017cfb2bb6da398aff03a4027bcf1ef89272
NoopDoge <irolori64@gmail.com>
2020-02-29 17:44:05 +0100

    passing input values works now for all configuration (pattern_len * n_neuron = n_inputs)

--- src/MultilayerPerceptron.py
+++ src/MultilayerPerceptron.py
@@ -16,7 +16,6 @@ class Multilayerperceptron:
                     self.neurons[i] = [n]
                 else:
                     self.neurons[i].append(n)
-        print(self.neurons)
         key_list = list(self.neurons.keys())
         for key_prime in key_list:
             if key_list[-1] != key_prime:
@@ -30,21 +29,27 @@ class Multilayerperceptron:
                     neuron2 = self.neurons[key_secondary][j]
 
                     if key_prime == 0:
-                        for h in range(n_inputs):
-                            neuron1.set_input_cnts(Connector.Connector(None, neuron1))
+                        if len(neuron1.get_input_cnts()) < n_inputs:
+                            for h in range(n_inputs):
+                                neuron1.set_input_cnts(Connector.Connector(None, neuron1))
 
                     elif key_prime == key_list[-1]:
-                        c = Connector.Connector(neuron1, neuron2)
+                        c = Connector.Connector(neuron1, None)
                         neuron1.add_output(c)
-                        neuron2.set_input_cnts(c)
                         break
+
 
                     c = Connector.Connector(neuron1, neuron2)
                     neuron1.add_output(c)
                     neuron2.set_input_cnts(c)
 
+    def dbg_prnt_top(self):
+        print(self.neurons)
+
+
     def train(self, train_dict):
         sample = None
+        self.dbg_prnt_top()
         for k in list(train_dict.values()):
             for l in k:
                 for a in range(len(list(self.neurons.keys()))):
@@ -53,31 +58,41 @@ class Multilayerperceptron:
                         cnts = n.get_input_cnts()
                         otps = n.get_output_cnts()
                         if a == 0:
+                            if len(self.neurons[a]) > 1:
+                                cnts = []
+                                for neuron in self.neurons[a]:
+                                    for c in neuron.get_input_cnts():
+                                        cnts.append(c)
                             if len(self.neurons[a]) * self.n_inputs == len(l):
-                                sample = l
-                                #print(sample)
-                                for o in range(len(l)):
-                                    n.clear_input()
-                                    cnts[o].set_input_value(l[o])
 
-                                    n.fetch_input()
-                                    for o in otps:
-                                        o.set_input_value(n.generate_output())
-    
+                                for o in range(len(l)):
+                                    sample = l[o]
+                                    otps = cnts[o].get_output().get_output_cnts()
+                                    #print(otps)
+                                    cnts[o].get_output().clear_input()
+                                    cnts[o].set_input_value(l[o])
+                                    #print("{}  {}  {}".format(l, cnts[o].get_input_value(), n))
+                                    cnts[o].get_output().fetch_input()
+                                    for ot in otps:
+                                        ot.set_input_value(cnts[o].get_output().generate_output())
+
                             else:
                                 raise Exception ("Input Dimensions must match topography")
 
+                        #else:
                         n.clear_input()
                         n.fetch_input()
+                        print("{} {} {} {}".format(a, b, n.input ,n.generate_output()))
+
                         for o in otps:
                             o.set_input_value(n.generate_output())
 
-                print("{}  {}".format(sample, self.neurons[list(self.neurons.keys())[1]][-1].generate_output()))
+               # print("{}  {}".format(sample, self.neurons[list(self.neurons.keys())[1]][-1].generate_output()))
 
 
 
 
-m = Multilayerperceptron(2, 2, [1, 1, 1])
+m = Multilayerperceptron(1, 2, [2, 3, 1])
 
 m.train(
     {1: [(1, 0), (0, 1)], 0: [(1, 1), (0, 0)]}
