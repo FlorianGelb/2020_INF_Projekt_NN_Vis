0f51103095f2df72c13cd2b91004144dea104ac3
NoopDoge <irolori64@gmail.com>
2020-03-22 17:50:04 +0100

    some cosmetical things

--- src/exp_pygame/test.py
+++ src/exp_pygame/test.py
@@ -4,24 +4,27 @@ import src.exp_pygame.Neuron as Neuron
 import src.exp_pygame.Connector as Connector
 import src.exp_pygame.Button as Button
 import src.exp_pygame.InputTerminal as Terminal
-
+import random
 
 class Visualizer(MLP.Multilayerperceptron):
     def __init__(self, n, eta, layers, height, width):
         MLP.Multilayerperceptron.__init__(self, n, eta, layers)
         pygame.init()
-        self.total_error = [0]
-        self.alpha = 0.01
+        self.total_error = []
+        self.alpha = 0.1
         self.a =  0
         self.b = 0
         self.s = 20
         self.off = 0
-        self.ef = "MAE"
+        self.off = 0
+        self.digit = 4
+        self.ef = "RMSE"
         self.train_dict = {1:[(0,1) ,(1, 0), (1,1)], 0:[(0,0)]}
         self.eta = eta
         self.height = height
         self.width = width
-        self.win = pygame.display.set_mode((self.width, self.height), pygame.RESIZABLE, pygame.RESIZABLE or pygame.NOFRAME)
+        self.win = pygame.display.set_mode((self.width, self.height), pygame.RESIZABLE)
+        self.surface = pygame.Surface((self.width, self.height))
         self.viz_neurons = []
         self.connections = []
         self.update_nn()
@@ -32,12 +35,16 @@ class Visualizer(MLP.Multilayerperceptron):
         started = False
         key_index = 0
         value_index = 0
+        #key = random.choice(list(self.train_dict.keys()))
+        #val = random.choice(self.train_dict[key])
+
         while window:
             if key_index >= len(list(self.train_dict.keys())):
                 key_index = 0
 
             key = list(self.train_dict.keys())[key_index]
             val = self.train_dict[key][value_index]
+
 
             for event in pygame.event.get():
                 mx, my = pygame.mouse.get_pos()
@@ -50,6 +57,7 @@ class Visualizer(MLP.Multilayerperceptron):
                     self.width = event.w
                     self.height = event.h
                     self.update_nn()
+
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if mx > self.button.get_x() and mx < self.button.get_colliding_x() and my > self.button.get_y() and my < self.button.get_colliding_y():
                         started = True
@@ -57,28 +65,32 @@ class Visualizer(MLP.Multilayerperceptron):
                         self.update_nn()
             if started:
 
-                    self.train(val, key)
+                self.train(val, key)
 
-
-
-            if self.error_function(self.total_error) <= self.alpha:
-                if value_index < len(self.train_dict[key_index]):
-                    value_index += 1
-                else:
-                    value_index = 0
-
-                if key_index < len(list(self.train_dict.keys())):
-                    if value_index > len(self.train_dict[key_index]) - 1:
-                        key_index += 1
-                        value_index = 0
+                #if self.error_function(self.total_error) <= self.alpha:
+                if self.error_function([self.total_error[-1]]) <= self.alpha:
+                    #key = random.choice(list(self.train_dict.keys()))
+                    #val = random.choice(self.train_dict[key])
+                    if value_index < len(self.train_dict[key_index]):
+                        value_index += 1
                     else:
-                        pass
-                else:
-                    key_index = 0
+                        value_index = 0
 
+                    if key_index < len(list(self.train_dict.keys())):
+                        if value_index > len(self.train_dict[key_index]) - 1:
+                            key_index += 1
+                            value_index = 0
+                        else:
+                            pass
+                    else:
+                        key_index = 0
 
+                    if key == list(self.train_dict.keys())[-1] and val == self.train_dict[list(self.train_dict.keys())[-1]][-1]:
+                        if self.error_function(self.total_error) < self.alpha:
+                            started = False
+                            print(self.total_error)
+                        #self.total_error = []
             self.update_nn()
-            self.total_error = [0]
 
 
     def create(self):
@@ -88,27 +100,31 @@ class Visualizer(MLP.Multilayerperceptron):
 
     def create_UI(self):
         self.button = Button.Button((10, 25), "Start", (200,20), (255,0,0))
-        self.button.draw(self.win)
+        self.button.draw(self.surface)
+
+        try:
+            text = str(self.error_function(self.total_error))
+        except Exception:
+            text = "0"
 
         font = pygame.font.SysFont("Arial", 15)
-        text = font.render(str(self.error_function(self.total_error)), True, (255, 255, 255))
-        self.win.blit(text, (100, 100))
+        text = font.render(text, True, (255, 255, 255))
+        self.surface.blit(text, (10, 50))
 
     def get_size(self):
         l,m = self.get_layer_max()
         off = (self.width / (1.06 * (len(list(self.neurons.keys())) - 1)))
         off_2 = (self.height / (m * 0.06 + (l -1)))
-        self.b = ((l * self.s) + ((l - 2) * off)) / 2
-        self.a = ((len(list(self.neurons.keys())) * self.s) + ((len(list(self.neurons.keys())) - 1) * off)) / 2
-
-        print(self.a, self.b)
 
         if off > off_2:
+            self.s = int(self.off * 0.06)
             self.off = off_2
-            self.s = int(self.off * 0.06)
         else:
-            self.off = off
             self.s = int(self.off * 0.06)
+            self.off = off
+
+        self.b = self.off / len(self.neurons[0])
+        self.a = ((len(list(self.neurons.keys())) * self.s) + ((len(list(self.neurons.keys())) - 1) * self.off)) / 2
 
     def get_layer_max(self):
         ln = 0
@@ -126,8 +142,8 @@ class Visualizer(MLP.Multilayerperceptron):
                 for neuron in self.neurons[key]:
                     index = self.neurons[key].index(neuron)
                     pos = (len(self.neurons[key]) / 2) * self.off
-                    x = int((key * self.off) + (self.width / 2) - self.a)
-                    y = int((pos - index * self.off) + (self.height  / 2) - self.b / 2)
+                    x = int((key * self.off) + (self.surface.get_size()[0] / 2) - self.a)
+                    y = int((pos - index * self.off) + (self.surface.get_size()[1] / 2) - self.b)# - self.b / 2)
 
                     c = (255, 0, 0)
                     if neuron.bias:
@@ -136,7 +152,7 @@ class Visualizer(MLP.Multilayerperceptron):
                     if key == list(self.neurons.keys())[-1] or key == list(self.neurons.keys())[0]:
                         self.viz_neurons.append(Terminal.InputTerminal(x,y, str(neuron.input),c, self.s, self.s))
                     else:
-                        self.viz_neurons.append(Neuron.Neuron(x, y, str(neuron.output), c, self.s))
+                        self.viz_neurons.append(Neuron.Neuron(x, y, str(round(neuron.output, self.digit)), c, self.s))
 
             for key in list(self.neurons.keys()):
                 for neuron in self.neurons[key]:
@@ -147,22 +163,26 @@ class Visualizer(MLP.Multilayerperceptron):
                                     end = (n.get_x(), n.get_y())
                                 if cnt.get_input_neuron().n_id == n.get_id():
                                     start = (n.get_x(), n.get_y())
-                            self.connections.append(Connector.Connector(start, end, (255, 0, 0), str(cnt.get_weight())))
+                            self.connections.append(Connector.Connector(start, end, (255, 0, 0), str(round(cnt.get_weight(), self.digit))))
 
     def draw(self):
         for neuron in self.viz_neurons:
-            neuron.draw(self.win)
+            neuron.draw(self.surface)
 
         for c in self.connections:
-            c.draw(self.win)
+            c.draw(self.surface)
 
     def update_nn(self):
-        self.win = pygame.display.set_mode((self.width, self.height), pygame.RESIZABLE or pygame.NOFRAME)
+        self.win = pygame.display.set_mode((self.width, self.height),pygame.RESIZABLE)
+        self.surface = pygame.Surface((self.width, self.height))
+        self.surface.fill((0,0,0))
         self.viz_neurons = []
         Neuron.Neuron.id = 0
         self.connections = []
         self.create()
         self.draw()
+        self.surface.convert()
+        self.win.blit(self.surface, (0, 0))
         pygame.display.update()
 
     def pass_values(self, key, value):
@@ -220,13 +240,15 @@ class Visualizer(MLP.Multilayerperceptron):
 
     def error_function(self, total_error):
         try:
-            total_error = total_error[1:]
+            print(total_error)
+
             if self.ef == "MSE":
                 return self.MSE(total_error)
             if self.ef == "MAE":
                 return self.MAE(total_error)
             if self.ef == "RMSE":
                 return self.RMSE(total_error)
+
         except ZeroDivisionError:
             return 0
 
@@ -243,6 +265,7 @@ class Visualizer(MLP.Multilayerperceptron):
 
     def MAE(self, total_error):
         return (1/len(total_error) * (sum([abs(e) for e in total_error])))
+
 
     def train(self, inp, expected):
         self.output = self.pass_values(expected, inp)
